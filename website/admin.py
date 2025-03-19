efe5f8692a8b5a492fcb07c49d2bbcd90e360841
Md Ariful Amin <arifulamindu@gmail.com>
2025-03-19 13:41:15 +0100

    simple example fo curd exercise

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import PersonalDetails
 
-# Register your models here.
+admin.site.register(PersonalDetails)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class PersonalDetails(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=12)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
